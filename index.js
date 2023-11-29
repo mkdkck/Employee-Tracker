843791eb03efb531984519e679a813f1aa25240c
mkdkck <colin173@gmail.com>
2023-11-29 23:39:24 +1030

    added more sturctured files. use .env to store the mysql login info

--- index.js
+++ index.js
@@ -1,21 +1,41 @@
 const inquirer = require ('inquirer');
-const mysql = 
+const mysql = require('mysql2');
+require('dotenv').config();
 
-const options = () => {
-    const questions = () => {
-        return inquirer.prompt([
-          {
-            type: 'list',
-            name: 'option',
-            message: 'What would you like to do?',
-            choices: ['View all department',
-              'View all roles',
-              'View all employees',
-              'Add a department',
-              'Add a role',
-              'Add an employee',
-              'Update an employee role']
-          },
-      ])
-    }
+const db = mysql.createConnection(
+  {
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: 'company_db',
+  },
+  console.log(`Connected to the company_db database.`),
+);
+
+const questions = () => {
+    return inquirer.prompt([
+      {
+        type: 'list',
+        name: 'option',
+        message: 'What would you like to do?',
+        choices: ['View all department',
+          'View all roles',
+          'View all employees',
+          'Add a department',
+          'Add a role',
+          'Add an employee',
+          'Update an employee role']
+      },
+  ])
 };
+
+
+
+
+
+
+
+
+
+questions();
+
+
