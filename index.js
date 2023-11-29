176aab6034468af0076c5d7de03cf444148db5b8
mkdkck <colin173@gmail.com>
2023-11-30 00:08:45 +1030

    changed inquirer prompt, assign the user choice to a variable for further sql operations

--- index.js
+++ index.js
@@ -1,42 +1,29 @@
 const inquirer = require ('inquirer');
 const mysql = require('mysql2');
+let inquireRes;
 require('dotenv').config();
 
-const db = mysql.createConnection(
-  {
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: 'company_db',
-  },
-  console.log(`Connected to the company_db database.`),
-);
-
-const questions = () => {
-    return inquirer.prompt([
-      {
-        type: 'list',
-        name: 'option',
-        message: 'What would you like to do?',
-        choices: ['View all department',
-          'View all roles',
-          'View all employees',
-          'Add a department',
-          'Add a role',
-          'Add an employee',
-          'Update an employee role',
-          'Exit']
-      },
-  ])
-};
-
-
-
-
-
-
-
-
-
-questions();
+// const db = mysql.createConnection(
+//   {
+//     user: process.env.DB_USER,
+//     password: process.env.DB_PASSWORD,
+//     database: 'company_db',
+//   },
+//   console.log(`Connected to the company_db database.`),
+// );
+const options =()=> inquirer.prompt(
+  { type: 'list',
+    name: 'options',
+    message: 'What would you like to do?',
+    choices: ['View all department',
+      'View all roles',
+      'View all employees',
+      'Add a department',
+      'Add a role',
+      'Add an employee',
+      'Update an employee role',
+      'Exit']
+})
+.then ((res)=> inquireRes=res.options)
 
 
