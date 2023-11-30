07d701d80baa558ca0365edecec66ffde62f2b18
mkdkck <colin173@gmail.com>
2023-12-01 01:04:39 +1030

    preset all the questions, now going into query section

--- SQLquery/department.js
+++ SQLquery/department.js
@@ -1,5 +1,5 @@
 const db = require('./config/connection');
-const inquirer = require ('inquirer');
+let dl;
 
 function viewAllDep () {
     db.query (`SELECT * FROM department;`,(err,results) =>{
@@ -8,9 +8,10 @@ function viewAllDep () {
 };
 
 function addADep(newDep) {
-   db.query (`INSERT INTO department (dep_name) VALUES (?)`,[newDep],(err,results) =>{
+   db.query (`INSERT INTO department (dep_name) VALUES (?)`,newDep,(err,results) =>{
     if (err) throw (err);
     console.log('successfully added a new department')}); 
 }
+
 
 module.exports = {viewAllDep,addADep};
--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const inquirer = require ('inquirer');
+const db = require('./SQLquery/config/connection');
 const {viewAllDep,addADep} = require('./SQLquery/department')
 const {viewAllEmp}=require ('./SQLquery/emp')
 const {viewAllEmpRole}=require ('./SQLquery/emp_role')
 let inquireRes;
+let depList;
+
+db.query (`SELECT dep_name FROM department`,(err,results)=>{
+  if (err) throw (err);
+  depList = results.map(result => result.dep_name)
+})
+
 
 const options = ()=> {inquirer.prompt([
   { type: 'list',
@@ -18,6 +26,8 @@ const options = ()=> {inquirer.prompt([
       'Update an employee role',
       'Exit'],
   },
+
+  //new department questions
   { type: 'input',
     name: 'newDep',
     message: 'Please insert the name of the new department',
@@ -25,11 +35,128 @@ const options = ()=> {inquirer.prompt([
         if (input) {
         return true;
         } else{
-        return 'Please type a name for the new department'}
+        return 'The input cannot be NULL'}
     },
-    when: (answers)=> 'Add a department' === answers.options,
+    when: (answers)=> answers.options === 'Add a department',
   },
-  
+
+  //new roles questions
+  { type: 'input',
+    name: 'newRoleTitle',
+    message: 'What is the new title',
+    validate: function(input){
+        if (input) {
+        return true;
+        } else{
+        return 'The input cannot be NULL'}
+    },
+    when: (answers)=> answers.options === 'Add a role',
+  },
+  { type: 'input',
+    name: 'newRoleSalary',
+    message: 'What is the salary',
+    validate: function(input){
+        if (input) {
+        return true;
+        } else{
+        return 'The input cannot be NULL'}
+    },
+    when: (answers)=> answers.newRoleTitle ? true:false,
+  },
+  { type: 'list',
+    name: 'newRoleDep',
+    message: 'What is the role`s department',
+    choices: depList,
+    validate: function(input){
+        if (input) {
+        return true;
+        } else{
+        return 'The input cannot be NULL'}
+    },
+    when: (answers)=> answers.newRoleTitle ? true:false,
+  },
+
+  //new employee questions
+  { type: 'input',
+    name: 'first_name',
+    message: 'What is the new employee`s first name',
+    validate: function(input){
+        if (input) {
+        return true;
+        } else{
+        return 'The input cannot be NULL'}
+    },
+    when: (answers)=> answers.options === 'Add an employee',
+  },
+  { type: 'input',
+    name: 'last_name',
+    message: 'What is the new employee`s last name',
+    validate: function(input){
+        if (input) {
+        return true;
+        } else{
+        return 'The input cannot be NULL'}
+    },
+    when: (answers)=> answers.first_name ? true:false,
+  },
+  { type: 'input',
+    name: 'role_id',
+    message: 'What is the title',
+    validate: function(input){
+        if (input) {
+        return true;
+        } else{
+        return 'The input cannot be NULL'}
+    },
+    when: (answers)=> answers.first_name ? true:false,
+  },
+  { type: 'input',
+    name: 'manager_id',
+    message: 'Who is the manager',
+    validate: function(input){
+        if (input) {
+        return true;
+        } else{
+        return 'The input cannot be NULL'}
+    },
+    when: (answers)=> answers.first_name ? true:false,
+  },
+
+  //Update an employee role
+  { type: 'input',
+    name: 'updateRoleTitle',
+    message: 'Which role title you want to update',
+    validate: function(input){
+        if (input) {
+        return true;
+        } else{
+        return 'The input cannot be NULL'}
+    },
+    when: (answers)=> answers.options === 'Update an employee role',
+  },
+  { type: 'input',
+    name: 'updateRoleSalary',
+    message: 'What is the updated salary',
+    validate: function(input){
+        if (input) {
+        return true;
+        } else{
+        return 'The input cannot be NULL'}
+    },
+    when: (answers)=> answers.updateRoleTitle ? true:false,
+  },
+  { type: 'input',
+    name: 'updatedRoleDep',
+    message: 'What is the updated role`s department',
+    validate: function(input){
+        if (input) {
+        return true;
+        } else{
+        return 'The input cannot be NULL'}
+    },
+    when: (answers)=> answers.updateRoleTitle ? true:false,
+  },
+
   
 
 ])
@@ -54,7 +181,7 @@ const SQLquery =()=> {
       addADep(inquireRes.newDep)
     break;
     case "Add a role":
-  
+
     break;
     case "Add an employee":
   
